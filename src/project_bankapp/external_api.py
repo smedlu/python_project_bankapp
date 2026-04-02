a35052e814b8979df95aa0fcf052fcdf79cb0fff
smedlu <zav80@mail.ru>
2026-04-02 13:13:27 +0500

    style: fix last flake8 issues

--- src/project_bankapp/external_api.py
+++ src/project_bankapp/external_api.py
@@ -3,25 +3,32 @@ import requests
 from dotenv import load_dotenv
 from typing import Any
 
-# Загружаем переменные из .env
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
 
 
 def convert_to_rub(transaction: dict[str, Any]) -> float:
     """Извлекает сумму и конвертирует в рубли, если валюта USD или EUR."""
-    amount = float(transaction.get("operationAmount", {}).get("amount", 0))
-    currency = transaction.get("operationAmount", {}).get("currency", {}).get("code")
+    amount_data = transaction.get("operationAmount", {})
+    amount = float(amount_data.get("amount", 0))
+    currency = amount_data.get("currency", {}).get("code")
 
     if currency == "RUB":
         return amount
 
     if currency in ["USD", "EUR"]:
-        url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={currency}&amount={amount}"
+        url = "https://api.apilayer.com/exchangerates_data/convert"
+        params = {"to": "RUB", "from": currency, "amount": amount}
         headers = {"apikey": API_KEY}
 
         try:
-            response = requests.get(url, headers=headers, timeout=10)
+            response = requests.get(
+                url,
+                params=params,
+                headers=headers,
+                timeout=10
+
+            )
             response.raise_for_status()
             data = response.json()
             return float(data.get("result", 0))
